4b4875fa76be089be50d283a247ff285faf45b8d
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2022-11-30 21:11:49 +0000

    Initial Commit

---
+++ Recap/RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapApp.swift
+//  Recap
+//
+//  Created by Kaylee Williams on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
